986c3464292fee15ec351a23ca9821d5a96ed6d8
Aleksandar <aleksandarstaletovic99>
2022-01-09 16:51:11 +0100

    Ubacivanje liste svih filmova

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import NavBar from './components/NavBar';
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-
+import {useState} from 'react';
 function App() {
+  const [filmovi,setFilmovi] = useState([
+    {
+      id: 1,
+      title: "Gospodar prstenova",
+      description:
+        "Gospodar prstenova je roman epske fantastike koji je napisao engleski akademik i filolog Džon Ronald Rejel Tolkin",
+        price: 100,
+        amount: 0,
+        money:0,
+        
+        img:0
+    },
+    {
+      id: 2,
+      title: "Paklene ulice",
+      description:
+        "Paklene ulice američka je franšiza koja uključuje niz akcionih filmova o ilegalnim uličnim auto-trkama i pljačkama kojima se bave glavni likovi.",
+        price: 200,
+        amount: 0,
+        money:0,
+        img:0 
+    },
+    {
+      id: 3,
+      title: "Kum",
+      description:
+        "Kum je američki dramski film režisera Fransisa Forda Kopole iz 1972. godine s Marlonom Brandom, Alom Pačinom, Džejmsom Kanom, Robertom Duvalom i Dajanom Kiton.",
+        price: 300,
+        amount: 0,
+        money:0,
+        img: 0
+    },
+    {
+      id: 4,
+      title: "Spajdermen",
+      description:
+        "Spajdermen je američki film iz 2002. godine zasnovan na Marvelovom strip junaku Spajdermenu.",
+        price: 300,
+        amount: 0,
+        money:0,
+        img:  0
+    },
+   
+  ]);
   return(
     <BrowserRouter className="App">
         <NavBar ></NavBar>
