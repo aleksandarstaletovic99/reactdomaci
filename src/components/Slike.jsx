4a3b14755315d0bef01615f665d7c935344743fb
Aleksandar <aleksandarstaletovic99>
2022-01-09 19:38:48 +0100

    Ubacivanje hover funkcionalnosti

--- src/components/Slike.jsx
+++ src/components/Slike.jsx
@@ -13,32 +13,38 @@ function Slike()  {
      <div className='roditelj'>
          <div id='div1'>
          <img className="imag"  src={slika11}
-        
+         onMouseOver={e => (e.currentTarget.src = slika16)}
+         onMouseOut={e => (e.currentTarget.src = slika11)}
           />
          </div>
          <div id='div2'>
          <img className="imag"  src={slika12}
-         
+          onMouseOver={e => (e.currentTarget.src = slika15)}
+          onMouseOut={e => (e.currentTarget.src = slika12)}
          />
          </div>
          <div id='div3'>
          <img className="imag"  src={slika13}
-          
+          onMouseOver={e => (e.currentTarget.src = slika14)}
+          onMouseOut={e => (e.currentTarget.src = slika13)}
          />
          </div>
          <div id='div4'>
          <img className="imag"  src={slika14}
-         
+          onMouseOver={e => (e.currentTarget.src = slika13)}
+          onMouseOut={e => (e.currentTarget.src = slika14)}
          />
          </div>
          <div id='div5'>
          <img className="imag"  src={slika15}
-         
+          onMouseOver={e => (e.currentTarget.src = slika12)}
+          onMouseOut={e => (e.currentTarget.src = slika15)}
          />
          </div>
          <div id='div6'>
          <img className="imag"  src={slika16}
-          
+          onMouseOver={e => (e.currentTarget.src = slika11)}
+          onMouseOut={e => (e.currentTarget.src = slika16)}
          />
          </div>
      </div>  
