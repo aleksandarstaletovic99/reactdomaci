5ebe47458713b1f4219fe9ed7dee8cdacbba7603
Aleksandar <aleksandarstaletovic99>
2022-01-09 16:47:10 +0100

    Ubacivanje ruta

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   return(
     <BrowserRouter className="App">
         <NavBar ></NavBar>
+    <Routes>
+      <Route path="/"/>
+      <Route path="/cart"/>
+      <Route path="/slike"/>
+    </Routes>
     </BrowserRouter>
 
   );
